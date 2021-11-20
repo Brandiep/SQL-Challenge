a67c554bfb7d0b5c7c0ffb27a69c654dbf8c9662
Brandie Parker <brandie.parker27@gmail.com>
2021-11-19 22:59:22 -0500

    uploading SQL files

---
+++ EmployeeSQL/Tables.sql
@@ -0,0 +1,69 @@
+-- Create department table and import CSV
+drop table departments;
+
+create table departments(
+dept_no VARCHAR NOT NULL,
+dept_name VARCHAR(30) NOT NULL,
+PRIMARY KEY(dept_no));
+
+SELECT * FROM departments;
+
+-- Create titles table and import CSV
+drop table titles;
+
+create table titles(
+title_id VARCHAR(30) NOT NULL,
+title VARCHAR(30) NOT NULL,
+PRIMARY KEY(title_id));
+
+SELECT * FROM titles;
+
+-- Create employees table and import CSV
+drop table employees;
+
+create table employees(
+emp_no VARCHAR(30) NOT NULL,
+emp_title VARCHAR(30) NOT NULL,
+birth_date VARCHAR(10) NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+sex VARCHAR(30) NOT NULL,
+hire_date VARCHAR(10) NOT NULL,
+FOREIGN KEY (emp_title) REFERENCES titles(title_id),
+PRIMARY KEY(emp_no));
+
+SELECT * FROM employees;
+
+-- Create dept_manager table and import CSV 
+drop table dept_manager;
+
+create table dept_manager(
+dept_no VARCHAR(30) NOT NULL,
+emp_no VARCHAR(30) NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
+
+SELECT * FROM dept_manager;
+
+-- Create salaries table and import CSV 
+drop table salaries;
+
+create table salaries(
+emp_no VARCHAR(30) NOT NULL,
+salary INTEGER NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
+
+SELECT * FROM salaries;
+
+-- Create dept_emp table and import CSV 
+drop table dept_emp;
+
+create table dept_emp(
+emp_no VARCHAR(30) NOT NULL,
+dept_no VARCHAR(30) NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+PRIMARY KEY(emp_no,dept_no));
+
+SELECT * FROM dept_emp;
+
